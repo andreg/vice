3240ad1a77aabea80d28ca14c3b9f9dcbf316e4a
Andrea Gandino <andreagandino@gmail.com>
2025-11-28 12:57:37 +0100

    .

--- src/VicePlugin.php
+++ src/VicePlugin.php
@@ -2,39 +2,64 @@
 
 namespace Andreg\Vice;
 
+use Andreg\Vice\Enums\NavigationPosition;
+use Andreg\Vice\Support\Color;
 use Filament\Contracts\Plugin;
+use Filament\Enums\UserMenuPosition;
 use Filament\FontProviders\LocalFontProvider;
 use Filament\Panel;
-use Filament\Support\Colors\Color;
 
 class VicePlugin implements Plugin {
 
-	public static function make(): static {
-		return new static();
-	}
+	private array $config = [];
 
 	public function getId(): string {
 		return 'vice';
 	}
 
-	public function register( Panel $panel ): void {
-		$panel
-			// ->topNavigation()
-			->font(
-				family: 'InterVariable',
-				url: asset( 'vendor/andreg/vice/fonts/inter.css' ),
-				provider: LocalFontProvider::class
-			)
-			->colors( function () {
-				return [
-					'primary' => Color::Slate,
-					'gray'    => Color::Slate,
-				];
-			} )
-			->viteTheme( 'vendor/andreg/vice/resources/css/theme.css' );
+	public static function make( array $config = [] ): static {
+		$instance = new static();
+		$instance->setConfig( $config );
+
+		return $instance;
 	}
 
-	public function boot( Panel $panel ): void {
+	public function setConfig( array $config ): static {
+		$this->config = $config;
+
+		return $this;
 	}
+
+	public function register( Panel $panel ): void {
+		$defaultColors = [
+			'danger'  => Color::Red,
+			'gray'    => Color::Zinc,
+			'info'    => Color::Blue,
+			'primary' => Color::Blue,
+			'success' => Color::Green,
+			'warning' => Color::Amber,
+		];
+
+		$panel->colors( array_merge( $defaultColors, $this->config[ 'colors' ] ?? [] ) );
+
+		if ( ! empty( $this->config[ 'navigation' ] ) && NavigationPosition::Topbar === $this->config[ 'navigation' ] ) {
+			$panel->topNavigation( true );
+			$panel->userMenu( position: UserMenuPosition::Topbar );
+		}
+		else {
+			$panel->topNavigation( false );
+			$panel->userMenu( position: UserMenuPosition::Sidebar );
+		}
+
+		$panel->font(
+			family: 'InterVariable',
+			url: asset( 'vendor/andreg/vice/fonts/inter.css' ),
+			provider: LocalFontProvider::class
+		);
+
+		$panel->viteTheme( 'vendor/andreg/vice/resources/css/theme.css' );
+	}
+
+	public function boot( Panel $panel ): void {}
 
 }
